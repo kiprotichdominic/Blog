a7bb8f80253c905aa8e5f5736ca61f4175bae8e4
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-26 21:54:00 +0300

    create forms

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts= [
-    {
-        "author": "Kiprotich",
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2019'
-    },
-        {
-        "author": "Kiprotich Dommie",
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 27, 2018'
-    }
-]
-
+from .models import Post
 
 def home(request):
     context={
-        'posts':posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/index.html', context)
 
