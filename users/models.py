5fcca9b7a39435780f9775fb2a8caad87e6f94c3
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-27 14:51:01 +0300

    create profile for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # CASCADE means that when a user is deleted a profile is deleted and when profile is deleted user is not deleled
+    image = models.ImageField(default ='default.jpg', upload_to='profile_pics')
+    
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
